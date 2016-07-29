e59c2353840a3579f77f185f83fb30578c15eb4b
Adam Jacques <adam@technowizardry.net>
2016-07-29 08:53:47 -0700

    Include DAC_OVERRIDE capability
    This is required to enable us to write
    to any file/folder regardless of permissions.
    
    Some cert folders may be read only by a specific user

--- agent_container.rb
+++ agent_container.rb
@@ -3,7 +3,14 @@ class AgentContainer
   def self.register(access_token:, bootstrap_url:, image_name:, other_containers: [])
     env = %W(BOOTSTRAP_URL=#{bootstrap_url} ACCESS_TOKEN=#{access_token})
     container = Docker::Container.create(Cmd: ['run'], Image: image_name, Binds: ['/:/host-volume'],
-                                         Volumes_From: other_containers, Env: env, CapDrop: ['ALL'], Name: '/soteria-agent')
+                                         Volumes_From: other_containers, Env: env, CapDrop: ['ALL'], CapAdd: capabilities, Name: '/soteria-agent')
     container.start
+  end
+
+  private
+
+  def capabilities
+    # Since the agent runs as root, we use this permission to ignore file owner/groups
+    ['DAC_OVERRIDE']
   end
 end
